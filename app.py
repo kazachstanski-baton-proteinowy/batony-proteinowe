fa41a5cbef9f070cecaccdf67926b1fe9a151e6a
student-wroclaw <lachr795@gmail.com>
2025-07-07 18:03:21 +0200

    Dodano menu nawigacyjne

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def index():
     ]
     return render_template('index.html', produkty=produkty)
 
+@app.route('/kontakt')
+def kontakt():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
